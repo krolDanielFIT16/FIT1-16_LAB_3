b7f231ca51ad72d39902b85ed1c7cfda73e8626c
Kusyaka <ender.redragon@gmail.com>
2022-09-16 11:19:01 +0300

    done

---
+++ p1.py
@@ -0,0 +1,6 @@
+a = float(input("А: "))
+b = float(input("B: "))
+c = float(input("C: "))
+
+print(f"a + b + c = {a+b+c}")
+print(f"a * b * c = {a*b*c}")
---
+++ p2.py
@@ -0,0 +1,5 @@
+import sys, math
+
+l = float(sys.argv[1])
+r = l / 2 * math.pi
+print(math.pi * (r ** 2))
